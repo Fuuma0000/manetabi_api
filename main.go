e3899ecdd2ac86cff5588ae3c615d81ef9a35723
Fuuma <masa81hiro69@gmail.com>
2023-07-31 01:11:06 +0900

    #1 Modelを分けた。sqlの完成

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/Fuuma0000/manetabi_api/model"
 	"github.com/labstack/echo"
@@ -29,33 +28,39 @@ func main() {
 
 	// APIサーバー起動
 	e := echo.New()
-	e.POST("/login", SignUp)
+	e.POST("/signup", SignUp)
 
 	// サーバーを開始
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-type User struct {
-	ID        uint      `json:"id"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
-
 func SignUp(c echo.Context) error {
-	user := User{}
+	// ユーザー情報を格納する構造体
+	user := model.User{}
+	// リクエストボディを構造体にバインド
 	if err := c.Bind(&user); err != nil {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
+
+	// パスワードのハッシュ化
 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
-	newUser := model.User{Email: user.Email, Password: string(hash)}
-	_, err = db.Exec("INSERT INTO users (email, password) VALUES (?, ?)", newUser.Email, newUser.Password)
+
+	// ユーザー情報をDBに登録
+	newUser := model.User{
+		UserName:         user.UserName,
+		Email:            user.Email,
+		Password:         string(hash),
+		ProfileImagePath: user.ProfileImagePath,
+	}
+	q := `INSERT INTO users (user_name, email, password, profile_image_path) VALUES (?, ?, ?, ?)`
+	_, err = db.Exec(q, newUser.UserName, newUser.Email, newUser.Password, newUser.ProfileImagePath)
+
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
+
 	return c.String(http.StatusOK, "Hello, World!")
 }
