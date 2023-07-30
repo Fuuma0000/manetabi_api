669f74c3087fdcee41cb9cf60c603eefe42206f7
Fuuma <masa81hiro69@gmail.com>
2023-07-31 01:13:02 +0900

    #1 モデルの修正

--- model/user.go
+++ model/user.go
@@ -4,13 +4,13 @@ import "time"
 
 // User ユーザーモデル
 type User struct {
-	ID           uint      `json:"id"`
-	UserName     string    `json:"user_name"`
-	Email        string    `json:"email"`
-	Password     string    `json:"password"`
-	ProfileImage string    `json:"profile_image"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	ID               uint      `json:"id"`
+	UserName         string    `json:"user_name"`
+	Email            string    `json:"email"`
+	Password         string    `json:"password"`
+	ProfileImagePath string    `json:"profile_image_path"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
 }
 
 type UserResponse struct {
