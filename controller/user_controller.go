99329c45251c2bc6a5ba448242de864b027feb83
Fuuma <masa81hiro69@gmail.com>
2023-08-01 18:45:15 +0900

    #3 Loginのcontroller実装

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Fuuma0000/manetabi_api/model"
@@ -10,6 +11,7 @@ import (
 
 type IUserController interface {
 	SignUp(c echo.Context) error
+	Login(c echo.Context) error
 }
 
 type userController struct {
@@ -25,6 +27,13 @@ func (uc *userController) SignUp(c echo.Context) error {
 	if err := c.Bind(&user); err != nil {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
+	b, message, err := uc.uu.CheckDuplicateEmail(user.Email)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	if b { // 重複あり
+		return c.JSON(http.StatusBadRequest, message)
+	}
 	userRes, err := uc.uu.SignUp(user)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
@@ -32,58 +41,16 @@ func (uc *userController) SignUp(c echo.Context) error {
 	return c.JSON(http.StatusCreated, userRes)
 }
 
-// func SignUp(c echo.Context) error {
-// 	// ユーザー情報を格納する構造体
-// 	user := model.User{}
-// 	// リクエストボディを構造体にバインド
-// 	if err := c.Bind(&user); err != nil {
-// 		return c.JSON(http.StatusBadRequest, err.Error())
-// 	}
-
-// 	// メールアドレスの重複チェック
-// 	b, message := checkDuplicateEmail(user.Email)
-// 	if b { // 重複あり
-// 		return c.JSON(http.StatusBadRequest, message)
-// 	}
-
-// 	// パスワードのハッシュ化
-// 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
-// 	if err != nil {
-// 		return c.JSON(http.StatusBadRequest, err.Error())
-// 	}
-
-// 	// ユーザー情報をDBに登録
-// 	newUser := model.User{
-// 		UserName:         user.UserName,
-// 		Email:            user.Email,
-// 		Password:         string(hash),
-// 		ProfileImagePath: user.ProfileImagePath,
-// 	}
-// 	q := `INSERT INTO users (user_name, email, password, profile_image_path) VALUES (?, ?, ?, ?)`
-// 	_, err = db.Exec(q, newUser.UserName, newUser.Email, newUser.Password, newUser.ProfileImagePath)
-
-// 	if err != nil {
-// 		return c.JSON(http.StatusBadRequest, err.Error())
-// 	}
-
-// 	return c.String(http.StatusOK, "Hello, World!")
-// }
-
-// // メールアドレスの重複チェック
-// func checkDuplicateEmail(email string) (bool, string) {
-// 	q := `SELECT COUNT(*) FROM users WHERE email = ?`
-// 	row := db.QueryRow(q, email)
-
-// 	var count int
-// 	err := row.Scan(&count)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		return true, "メールアドレスの重複チェックに失敗しました"
-// 	}
-
-// 	if count > 0 {
-// 		return true, "メールアドレスが重複しています"
-// 	}
-
-// 	return false, ""
-// }
+func (uc *userController) Login(c echo.Context) error {
+	user := model.User{}
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	fmt.Println("login")
+	if err := uc.uu.Login(user); err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	fmt.Println("login success")
+	// TODO:ログインが脆弱だから後でなんかする
+	return c.NoContent(http.StatusOK)
+}
