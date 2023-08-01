4ea13c34dbb8578b353caf04e6abfd3e232297bf
Fuuma <masa81hiro69@gmail.com>
2023-08-01 18:44:42 +0900

    #3 LogInのusecaseを実装

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"fmt"
+
 	"github.com/Fuuma0000/manetabi_api/infrastructure"
 	"github.com/Fuuma0000/manetabi_api/model"
 	"golang.org/x/crypto/bcrypt"
@@ -8,6 +10,8 @@ import (
 
 type IUserUsecase interface {
 	SignUp(user model.User) (model.UserResponse, error)
+	Login(user model.User) error
+	CheckDuplicateEmail(email string) (bool, string, error)
 }
 
 type userUsecase struct {
@@ -19,6 +23,7 @@ func NewUserUsecase(ui infrastructure.IUserInfrastructer) IUserUsecase {
 }
 
 func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
+	// TODO: メールアドレスのバリデーションをしないと空文字を許容してしまう
 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 	if err != nil {
 		return model.UserResponse{}, err
@@ -39,4 +44,28 @@ func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
 		ProfileImagePath: newUser.ProfileImagePath,
 	}
 	return resUser, nil
+}
+
+func (uu *userUsecase) Login(user model.User) error {
+	storedUser := model.User{}
+	fmt.Println("getuserbyemail start")
+	if err := uu.ui.GetUserByEmail(&storedUser, user.Email); err != nil {
+		return err
+	}
+	fmt.Println("GEtUserByEmail")
+	err := bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(user.Password))
+	fmt.Println("CompareHashAndPassword")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (uu *userUsecase) CheckDuplicateEmail(email string) (bool, string, error) {
+	b, str, err := uu.ui.CheckDuplicateEmail(email)
+	if err != nil {
+		fmt.Println(err)
+		return b, str, err
+	}
+	return b, str, nil
 }
