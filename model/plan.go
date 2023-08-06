75a2d1f7f025cd45b3ede23648079d150e6c6956
Fuuma <masa81hiro69@gmail.com>
2023-08-06 22:42:30 +0900

    #8 レスポンスを追加

--- model/plan.go
+++ model/plan.go
@@ -17,3 +17,18 @@ type Plan struct {
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
+
+type PlanResponse struct {
+	ID          uint      `json:"id"`
+	UserID      uint      `json:"user_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Thumbnail   string    `json:"thumbnail"`
+	Cost        uint      `json:"cost"`
+	LocationID  uint      `json:"location_id"`
+	StartDate   time.Time `json:"start_date"`
+	EndDate     time.Time `json:"end_date"`
+	IsPublic    bool      `json:"is_public"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
